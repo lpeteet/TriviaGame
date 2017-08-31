13c1054c78abab397f3061506dd3a41dd90e36ce
Larry Peteet <larrypv10@gmail.com>
2017-08-30 22:31:38 -0400

    May be the Final Version!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,11 +2,6 @@
 
 $(document).ready(function() {
     /* Global Variables */
-    var wins = 0;
-    var losses = 0;
-    var scoreToMatch = 0;
-    var theirScore = 0;
-    var gameOver = true;
     var number = 30;
     var intervalId; //Variable that will hold our interval ID when we execute
     
@@ -17,40 +12,60 @@ $(document).ready(function() {
         this.correctIdx = correctIdx;
     };
 
-/*  $.extend(Question.prototype, {
-        AssignNewValue: function() {
-            //Random between 1 and 12
-            this.points = Math.floor((Math.random() * 12) + 1);
-        }
-    });
- */
     var questionArray = [];
 
     questionArray.push(new Question("Fastest Animal on Earth?", ["Elephant", "Snail", "Gazelle", "Cheetah"], 4));
-    questionArray.push(new Question("Which Dimension is Time?", ["1", "2", "3", "4"], 4));
+    questionArray.push(new Question("Which Dimension is Time?", ["First", "Second", "Third", "Fourth"], 4));
     questionArray.push(new Question("Where do you find Loopholes?", ["Knitting", "Taxes", "Crochet", "Quantum Physics"], 2));
     questionArray.push(new Question("What is the Most Popular Auto Color in America?", ["White", "Black", "Yellow", "Green"], 1));
     questionArray.push(new Question("Which Is The Furthest From Earth?", ["Jupiter", "Uranus", "Pluto", "Titan"], 3));
 
     var currQuestion = 0;
     var currAnswer = 0;
+    $('.start').on("click", function() {
+        //alert("It Works and ID: " + $(this).attr('id'));
+        console.log("ReStart Button Clicked!");
+        StartGame();
+    });
 
     $("#answer1").on("click", function() {
         // alert("It Works and ID: " + $(this).attr('id'));
+        if (currAnswer > 0) {
+            //Currently Clicks are NOT Enabled
+            return;
+        }
         ResolveAnswer(1);
+        currAnswer = 1;
     });
     $("#answer2").on("click", function() {
         // alert("It Works and ID: " + $(this).attr('id'));
+        if (currAnswer > 0) {
+            //Currently Clicks are NOT Enabled
+            return;
+        }
         ResolveAnswer(2);
+        currAnswer = 2;
     });
     $("#answer3").on("click", function() {
         // alert("It Works and ID: " + $(this).attr('id'));
+        if (currAnswer > 0) {
+            //Currently Clicks are NOT Enabled
+            return;
+        }
         ResolveAnswer(3);
+        currAnswer = 3;
     });
     $("#answer4").on("click", function() {
         // alert("It Works and ID: " + $(this).attr('id'));
+        if (currAnswer > 0) {
+            //Currently Clicks are NOT Enabled
+            return;
+        }
         ResolveAnswer(4);
+        currAnswer = 4;
     });
+
+    //******** FUNCTIONS  ********/
 
     //  The RunTimer function sets an interval
     //  that runs the decrement function once a second.
@@ -59,7 +74,7 @@ $(document).ready(function() {
     }
 
     function DelayGame() {
-        setTimeout(NextQuestion, 3000);
+        setTimeout(NextQuestion, 4000); //4 Seconds
     }
 
     //  The stop function
@@ -89,53 +104,47 @@ $(document).ready(function() {
 
             //  Alert the user that time is up.
             $("#winlose").text("Time Up!!");
+            $("#winlose").css('color', 'red');
+            $("#winlose").blink({
+                delay: 200
+            });
             $("#winloseimg").attr('src', 'https://media.giphy.com/media/sFZ7jXF6KHu8g/giphy.gif');
+            $("#answer" + questionArray[currQuestion - 1].correctIdx).blink({
+                delay: 200
+            });
             DelayGame();
         }
     }
 
     function ResolveAnswer(answerNum) {
         StopTimer();
+
         if (answerNum == questionArray[currQuestion - 1].correctIdx) {
             //WINNER!
             //alert("GOOD JOB!!");
             $("#winlose").text("You Win!!");
+            $("#winlose").css('color', 'greenyellow');
             $("#winloseimg").attr('src', 'https://media.giphy.com/media/26tPcgtbhhbU88U2A/giphy.gif'); //<iframe src="https://giphy.com/embed/sFZ7jXF6KHu8g" width="480" height="354" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/sFZ7jXF6KHu8g">via GIPHY</a></p>
         } else {
             //alert("SORRY!");
             $("#winlose").text("Sorry, Incorrect!!");
+            //DEBUGGING
+            $("#winlose").css('color', 'red');
+            //$("#winlose").addclass("text-success");
+            //$("#winlose").addclass("text-danger");
             $("#winloseimg").attr('src', 'https://media.giphy.com/media/sFZ7jXF6KHu8g/giphy.gif');
             console.log("questionArray[currQuestion - 1].correctIdx", questionArray[currQuestion - 1].correctIdx);
-            switch(questionArray[currQuestion - 1].correctIdx) {
-                case 1:
-                    $("#answer1").blink({
-                        delay: 200
-                    });
-                    break;
-                case 2:
-                    $("#answer2").blink({
-                        delay: 200
-                    });
-                    break;
-                case 3:
-                    $("#answer3").blink({
-                        delay: 200
-                    });
-                    break;
-                default:
-                    $("#answer4").blink({
-                        delay: 200
-                    });
-            }; //Switch
-/*         $('#stop').click(function() {
-            $('.blink').unblink();
-        });
-        $('#stop2').click(function() {
-            $('.blink2').unblink();
-        });
- */
+            $("#answer" + questionArray[currQuestion - 1].correctIdx).blink({
+                delay: 200
+            });
         } // Else
+
+        $("#winlose").blink({
+            delay: 200
+        });
+
         DelayGame();
+
     } //function ResolveAnswer(answerNum) {
 
     function ShowQuestion() {
@@ -147,30 +156,27 @@ $(document).ready(function() {
         $("#answer4").text(questionArray[currQuestion - 1].answers[3]);
     }
 
-    function StartGame() {
-        currQuestion = 1;
-
-        ShowQuestion();
-
-        //  Set our number counter to 100.
-        number = 30;
-
-        //  Execute the run function.
-         RunTimer();
-    
-    };
-
     function NextQuestion() {
+        $("#winlose").unblink();
+        $("#winlose").css('color', 'white');
+        
+        
+        $("#answer1").unblink();
+        $("#answer2").unblink();
+        $("#answer3").unblink();
+        $("#answer4").unblink();
         currQuestion ++;
         
         //Done with All Questions?
-        if (currQuestion >= questionArray.length) {
+        if (currQuestion > questionArray.length) {
             //Done!
             StopTimer();
             alert("Thanks for Playing!");
             return;
         }
 
+        //Re-Enable Text Click Events
+        currAnswer = 0;
         ShowQuestion();
 
         //  Set our number counter to 100.
@@ -180,6 +186,21 @@ $(document).ready(function() {
 
         //  Execute the run function.
         RunTimer();
+    
+    };
+
+    function StartGame() {
+        currQuestion = 1;
+        if (intervalId > 0) {
+            StopTimer();
+        }
+        ShowQuestion();
+
+        //  Set our number counter to 100.
+        number = 30;
+
+        //  Execute the run function.
+         RunTimer();
     
     };
 
